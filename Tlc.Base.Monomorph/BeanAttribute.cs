95240f841f16fae612b4b79d2db8a10ded13ca74
Jerry Bowman <jerry.bowman@gmail.com>
2014-09-04 19:06:21 -0500

    2: Add Apache license headers to source files

--- Tlc.Base.Monomorph/BeanAttribute.cs
+++ Tlc.Base.Monomorph/BeanAttribute.cs
@@ -1,4 +1,25 @@
-﻿using System;
+﻿/***********************************************************************************************************************
+ *
+ * monomorph -- an open source .net simple dependency framework
+ * ============================================================
+ *
+ * Copyright (C) 2011-2014 by The Last Check, LLC
+ * https://github.com/jerrybowman/tlc.monomorph
+ *
+ ***********************************************************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ **********************************************************************************************************************/
+
+using System;
 
 namespace Tlc.Base.Monomorph
 {
